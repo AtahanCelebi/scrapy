3eec9e73d470cee22eac7a0f052162edcba7caed
atahan <39523757+AtahanCelebi@users.noreply.github.com>
2022-11-10 12:35:27 +0300

    mongodb

--- computerDeals/ComputerDeals/pipelines.py
+++ computerDeals/ComputerDeals/pipelines.py
@@ -6,8 +6,28 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+import pymongo
+import logging
 
+class MongodbPipeline(object):
+    collection_name = "all_deals" # under the database it refes collection name
 
-class ComputerdealsPipeline:
-    def process_item(self, item, spider):
+        
+
+    def open_spider(self,spider):
+        """
+        Go <Database Access> click "ADD NEW DATABASE USER"
+        it includes your userName and password
+        PS: Do not forget select "Built-in Role" --> Atlas admin or Read and write to any database authentication
+        Go <Network Access> click "ADD IP ADRESS" button
+        0.0.0.0/0   
+        """
+        self.client = pymongo.MongoClient("mongodb+srv://ata:ata@cluster0.3lic3mt.mongodb.net/?retryWrites=true&w=majority") #port of the database
+        self.db = self.client["ComputerDeals"] #database name
+
+    def close_spider(self,spider):
+        self.client.close()
+
+    def process_item(self,item,spider):
+        self.db[self.collection_name].insert(item)
         return item
